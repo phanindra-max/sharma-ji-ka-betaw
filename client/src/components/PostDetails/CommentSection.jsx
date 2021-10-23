bdaa535bc783acf783cc409538271f315b3c4fff
Kalaga Satya Phanindra Kumar <phanindra.1756.kkr@gmail.com>
2021-10-23 23:25:08 +0530

    Newwwww(★‿★)

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -1,45 +1,66 @@
-import React, { useState, useRef } from 'react';
-import { Typography, TextField, Button } from '@material-ui/core/';
-import { useDispatch } from 'react-redux';
+import React, { useState, useRef } from "react";
+import { Typography, TextField, Button } from "@material-ui/core/";
+import { useDispatch } from "react-redux";
 
-import { commentPost } from '../../actions/posts';
-import useStyles from './styles';
+import { commentPost } from "../../actions/posts";
+import useStyles from "./styles";
 
 const CommentSection = ({ post }) => {
-  const user = JSON.parse(localStorage.getItem('profile'));
-  const [comment, setComment] = useState('');
+  const user = JSON.parse(localStorage.getItem("profile"));
+  const [comment, setComment] = useState("");
   const dispatch = useDispatch();
   const [comments, setComments] = useState(post?.comments);
   const classes = useStyles();
   const commentsRef = useRef();
 
   const handleComment = async () => {
-    const newComments = await dispatch(commentPost(`${user?.result?.name}: ${comment}`, post._id));
+    const newComments = await dispatch(
+      commentPost(`${user?.result?.name}: ${comment}`, post._id)
+    );
 
-    setComment('');
+    setComment("");
     setComments(newComments);
 
-    commentsRef.current.scrollIntoView({ behavior: 'smooth' });
+    commentsRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <div>
       <div className={classes.commentsOuterContainer}>
         <div className={classes.commentsInnerContainer}>
-          <Typography gutterBottom variant="h6">Comments</Typography>
+          <Typography gutterBottom variant="h6">
+            Notes
+          </Typography>
           {comments?.map((c, i) => (
             <Typography key={i} gutterBottom variant="subtitle1">
-              <strong>{c.split(': ')[0]}</strong>
-              {c.split(':')[1]}
+              <strong>{c.split(": ")[0]}</strong>
+              {c.split(":")[1]}
             </Typography>
           ))}
           <div ref={commentsRef} />
         </div>
-        <div style={{ width: '70%' }}>
-          <Typography gutterBottom variant="h6">Write a comment</Typography>
-          <TextField fullWidth rows={4} variant="outlined" label="Comment" multiline value={comment} onChange={(e) => setComment(e.target.value)} />
+        <div style={{ width: "70%" }}>
+          <Typography gutterBottom variant="h6">
+            Handy Notes for your learning
+          </Typography>
+          <TextField
+            fullWidth
+            rows={4}
+            variant="outlined"
+            label="Make a list of important points"
+            multiline
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
           <br />
-          <Button style={{ marginTop: '10px' }} fullWidth disabled={!comment.length} color="primary" variant="contained" onClick={handleComment}>
+          <Button
+            style={{ marginTop: "10px" }}
+            fullWidth
+            disabled={!comment.length}
+            color="primary"
+            variant="contained"
+            onClick={handleComment}
+          >
             Comment
           </Button>
         </div>
