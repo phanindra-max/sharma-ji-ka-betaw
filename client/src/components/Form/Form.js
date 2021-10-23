27169331f89d216f36d800b30042ef50f9d93ea2
ishtiaqSamdani <ishmartishtiaq@gmail.com>
2021-10-23 04:07:04 -0800

    search button inline

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -7,6 +7,7 @@ import ChipInput from "material-ui-chip-input";
 
 import { createPost, updatePost } from "../../actions/posts";
 import useStyles from "./styles";
+import { Link } from "react-router-dom";
 
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
@@ -14,8 +15,8 @@ const Form = ({ currentId, setCurrentId }) => {
     message: "",
     tags: [],
     selectedFile: "",
-    aim:"",
-    resources:""
+    aim: "",
+    resources: "",
   });
   const post = useSelector((state) =>
     currentId
@@ -81,83 +82,13 @@ const Form = ({ currentId, setCurrentId }) => {
         onSubmit={handleSubmit}
       >
         <Typography variant="h6">
-          {currentId ? `Editing "${post?.title}"` : "Creating a Memory"}
+          {currentId ? `Editing "${post?.title}"` : "Creating a Track"}
         </Typography>
-        <TextField
-          name="title"
-          variant="outlined"
-          label="Title"
-          fullWidth
-          value={postData.title}
-          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
-        />
-        <TextField
-          name="Aim"
-          variant="outlined"
-          label="Aim"
-          fullWidth
-          value={postData.aim}
-          onChange={(e) => setPostData({ ...postData, aim: e.target.value })}
-        />
-        <TextField
-          name="Resources"
-          variant="outlined"
-          label="Resouces"
-          fullWidth
-          value={postData.resources}
-          onChange={(e) => setPostData({ ...postData, resources: e.target.value })}
-        />
-        <TextField
-          name="message"
-          variant="outlined"
-          label="Message"
-          fullWidth
-          multiline
-          rows={4}
-          value={postData.message}
-          onChange={(e) =>
-            setPostData({ ...postData, message: e.target.value })
-          }
-        />
-        <div style={{ padding: "5px 0", width: "94%" }}>
-          <ChipInput
-            name="tags"
-            variant="outlined"
-            label="Tags"
-            fullWidth
-            value={postData.tags}
-            onAdd={(chip) => handleAddChip(chip)}
-            onDelete={(chip) => handleDeleteChip(chip)}
-          />
-        </div>
-        <div className={classes.fileInput}>
-          <FileBase
-            type="file"
-            multiple={false}
-            onDone={({ base64 }) =>
-              setPostData({ ...postData, selectedFile: base64 })
-            }
-          />
-        </div>
-        <Button
-          className={classes.buttonSubmit}
-          variant="contained"
-          color="primary"
-          size="large"
-          type="submit"
-          fullWidth
-        >
-          Submit
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          size="small"
-          onClick={clear}
-          fullWidth
-        >
-          Clear
-        </Button>
+        <Link to="/form">
+          <button>
+            <div className="btn_text">Create</div>
+          </button>
+        </Link>
       </form>
     </Paper>
   );
