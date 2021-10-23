dd89249195f54857355feabad8d4fb85e600ed93
Kalaga Satya Phanindra Kumar <phanindra.1756.kkr@gmail.com>
2021-10-23 17:08:13 +0530

    Changed user model to have only unique mail addresses

--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  id: { type: String },
 });
 
 export default mongoose.model("User", userSchema);
