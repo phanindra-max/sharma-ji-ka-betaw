19429fa0274e54919744a94e3f8e8cbf067d0b39
Kalaga Satya Phanindra Kumar <phanindra.1756.kkr@gmail.com>
2021-10-23 17:03:17 +0530

    Url shifted to .env

--- server/index.js
+++ server/index.js
@@ -25,8 +25,6 @@ app.use("/topic", topicRouter);
 
 // console.log(process.env.NODE_ENV);
 
-const CONNECTION_URL =
-  "mongodb+srv://nashagri:y5tBsyg783gPa0e5@cluster0.jlnit.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
@@ -44,7 +42,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
