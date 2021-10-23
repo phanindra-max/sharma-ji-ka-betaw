a77beb833621dde7059e3b784bb4f2198c63e673
Kalaga Satya Phanindra Kumar <phanindra.1756.kkr@gmail.com>
2021-10-23 15:06:58 +0530

    Added the font

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API = axios.create({ baseURL: 'http://localhost:5000' });
 
-API.interceptors.request.use((req) => {
+axios.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${
       JSON.parse(localStorage.getItem("profile")).token
@@ -12,23 +12,23 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
-export const fetchPost = (id) => API.get(`/posts/${id}`);
-export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
+export const fetchPost = (id) => axios.get(`/posts/${id}`);
+export const fetchPosts = (page) => axios.get(`/posts?page=${page}`);
 export const fetchPostsByCreator = (name) =>
-  API.get(`/posts/creator?name=${name}`);
+  axios.get(`/posts/creator?name=${name}`);
 export const fetchPostsBySearch = (searchQuery) =>
-  API.get(
+  axios.get(
     `/posts/search?searchQuery=${searchQuery.search || "none"}&tags=${
       searchQuery.tags
     }`
   );
-export const createPost = (newPost) => API.post("/posts", newPost);
-export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+export const createPost = (newPost) => axios.post("/posts", newPost);
+export const likePost = (id) => axios.patch(`/posts/${id}/likePost`);
 export const comment = (value, id) =>
-  API.post(`/posts/${id}/commentPost`, { value });
+  axios.post(`/posts/${id}/commentPost`, { value });
 export const updatePost = (id, updatedPost) =>
-  API.patch(`/posts/${id}`, updatedPost);
-export const deletePost = (id) => API.delete(`/posts/${id}`);
+  axios.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`/posts/${id}`);
 
-export const signIn = (formData) => API.post("/user/signin", formData);
-export const signUp = (formData) => API.post("/user/signup", formData);
+export const signIn = (formData) => axios.post("/user/signin", formData);
+export const signUp = (formData) => axios.post("/user/signup", formData);
