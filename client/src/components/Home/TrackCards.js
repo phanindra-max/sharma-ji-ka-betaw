73121aefd8bd2adb6f31f5461ca742aa57735004
Krishna Sai Cholkar <krishnasai3cks@gmail.com>
2021-10-25 00:25:37 +0530

    Added sample Track

--- client/src/components/Home/TrackCards.js
+++ client/src/components/Home/TrackCards.js
@@ -1,19 +1,27 @@
-import React from "react";
-import "./TrackCards.css";
+import { Button } from "@material-ui/core"
+import React from "react"
+import "./TrackCards.css"
 
 function TrackCards(props) {
   // console.log(props.res);
+
   return (
     <>
       <div class="container">
         <div class="row">
-          {props.res.map((e) => (
-            <div class="col">
+          {props.res.map((e, index) => (
+            <div class="col" key={index}>
               <div className="card_head">
                 <div className="title_card">{e.trackName}</div>
                 <div className="Description">{e.description}</div>
                 <div className="outer_btn">
-                  <button className="btn_card">Show More</button>
+                  <Button
+                    type="link"
+                    href={"/track?id=" + e._id}
+                    className="btn_card"
+                  >
+                    Show More
+                  </Button>
                 </div>
               </div>
             </div>
@@ -21,7 +29,7 @@ function TrackCards(props) {
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default TrackCards;
+export default TrackCards
