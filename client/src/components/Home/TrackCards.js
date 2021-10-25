c642c5f1209d79269af2f87a125ee511c6ab9dd4
Kalaga Satya Phanindra Kumar <phanindra.1756.kkr@gmail.com>
2021-10-25 13:44:35 +0530

    Added track fetching screen

--- client/src/components/Home/TrackCards.js
+++ client/src/components/Home/TrackCards.js
@@ -1,19 +1,20 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 import "./TrackCards.css";
+import { Link } from "react-router-dom";
 
 function TrackCards(props) {
-  console.log(props.res);
-  console.log(props.search.to);
+  // console.log(props.res);
+  // console.log(props.search.to);
 
   if (props.search.length > 0) {
-    console.log("wdefwvrb");
+    // console.log("wdefwvrb");
     return (
       <>
         <div className="container">
           <div className="flexy">
-            {props.res.map((e) => 
-            (props.search===e.trackName)?(
+            {props.res.map((e) =>
+              props.search === e.trackName ? (
                 <div className="card_head">
                   <div className="title_card">{e.trackName}</div>
                   <div className="Description">{e.description}</div>
@@ -21,7 +22,10 @@ function TrackCards(props) {
                     <button className="btn_card">Show More</button>
                   </div>
                 </div>
-            ):(""))}
+              ) : (
+                ""
+              )
+            )}
           </div>
         </div>
       </>
@@ -32,13 +36,15 @@ function TrackCards(props) {
         <div className="container">
           <div className="flexy">
             {props.res.map((e) => (
-                <div className="card_head">
-                  <div className="title_card">{e.trackName}</div>
-                  <div className="Description">{e.description}</div>
-                  <div className="outer_btn">
+              <div className="card_head">
+                <div className="title_card">{e.trackName}</div>
+                <div className="Description">{e.description}</div>
+                <div className="outer_btn">
+                  <Link to={`/track/${e._id}`}>
                     <button className="btn_card">Show More</button>
-                  </div>
+                  </Link>
                 </div>
+              </div>
             ))}
           </div>
         </div>
--- client/src/components/Track/Track.js
+++ client/src/components/Track/Track.js
@@ -16,19 +16,11 @@ import useStyles from "./styles";
 import "./PostDetails.css";
 import { useState } from "react";
 import PencilImg from "./PencilImg.svg";
-const queryParams = new URLSearchParams(window.location.search);
+// const queryParams = new URLSearchParams(window.location.search);
 
-const Track = () => {
-  // const { post, posts, isLoading } = useSelector((state) => state.posts);
-  // const dispatch = useDispatch();
-  // const history = useHistory();
+const Track = ({ location }) => {
   const classes = useStyles();
-  const id = queryParams.get("id");
 
-  const dataFetch = async () => {
-    const resTrack = await axios.get(`/track/${id}`);
-    setTrack(resTrack.data);
-  };
   const [name, setName] = useState("");
   // const [subtopicArray, setSubtopicArray] = useState([])
   let subtopicArray = [];
@@ -36,24 +28,31 @@ const Track = () => {
   const [subtopic2, setSubtopic2] = useState("");
   const [subtopic3, setSubtopic3] = useState("");
   const [subtopic4, setSubtopic4] = useState("");
-  const [track, setTrack] = useState({});
+  const [trackInfo, setTrackInfo] = useState({});
+  const [topicInfo, setTopicInfo] = useState({});
 
-  const func = async () => {
-    subtopicArray = [subtopic1, subtopic2, subtopic3, subtopic4];
-    // console.log(subtopicArray);
-    // await axios.post(`/topics`)
-  };
+  const id = location.pathname.split("/")[2];
 
   useEffect(() => {
     dataFetch();
   }, []);
 
-  // const openPost = (_id) => history.push(`/posts/${_id}`);
+  const dataFetch = async () => {
+    const res = await axios.get(`/api/track/${id}`);
+    console.log(res.data);
+    setTrackInfo(res.data.trackDetails);
+    // setTopicInfo(res.data.topics);
+  };
 
-  // const recommendedPosts = posts.filter(({ _id }) => _id !== post._id);
+  const func = () => {
+    subtopicArray = [subtopic1, subtopic2, subtopic3, subtopic4];
+  };
 
   return (
     <>
+      {trackInfo ? "" : dataFetch}
+      {topicInfo ? "" : dataFetch}
+      {console.log(trackInfo)}
       <Paper style={{ padding: "20px", borderRadius: "15px" }} elevation={6}>
         <div
           class="modal fade"
@@ -168,22 +167,26 @@ const Track = () => {
 
             <div className={classes.section}>
               <Typography variant="h3" component="h2">
-                {track.trackName}
+                {trackInfo.trackName}
               </Typography>
               <Typography gutterBottom variant="body1" component="p">
-                {track.description}
+                {trackInfo.description}
               </Typography>
               <Typography variant="h6">
                 Created by:
-                <Link
-                  to={`/creators/${track.trackName}`}
-                  style={{ textDecoration: "none", color: "#3f51b5" }}
-                >
-                  {` ${track.trackName}`}
-                </Link>
+                {trackInfo.trackDetails ? (
+                  <Link
+                    to={`/creators/${trackInfo.trackName}`}
+                    style={{ textDecoration: "none", color: "#3f51b5" }}
+                  >
+                    {` ${trackInfo.trackDetails}`}
+                  </Link>
+                ) : (
+                  "Not Found"
+                )}
               </Typography>
               <Typography variant="body1">
-                {moment(track.createdAt).fromNow()}
+                {moment(trackInfo.trackDetails).fromNow()}
               </Typography>
               <Divider style={{ margin: "20px 0" }} />
               {/* <CommentSection post={post} /> */}
@@ -192,62 +195,28 @@ const Track = () => {
           </div>
 
           <div className="flow_chart">
-            <div className="head">Competive Coding</div>
-
-            {/* repeat this */}
-            <div className="block">
-              <div className="topic">
-                <div className="circle"></div>
-                <div className="outer_border">
-                  <div className="title border">
-                    Math
-                    <img className="pencil" src={PencilImg} />
-                  </div>
-                  <div className="sub_topics border">
-                    <div className="sub_topic">math1</div>
-                    <div className="sub_topic">math2</div>
-                    <div className="sub_topic">math3</div>
-                    <div className="sub_topic">math4</div>
+            <div className="head">{trackInfo.trackName}</div>
+            {topicInfo.map((e) => (
+              <div className="block">
+                <div className="topic">
+                  <div className="circle"></div>
+                  <div className="outer_border">
+                    <div className="title border">
+                      {e.name}
+                      <img className="pencil" src={PencilImg} />
+                    </div>
+                    <div className="sub_topics border">
+                      <div className="sub_topic">{e.subtopics[0]}</div>
+                      <div className="sub_topic">{e.subtopics[1]}</div>
+                      <div className="sub_topic">{e.subtopics[2]}</div>
+                      <div className="sub_topic">{e.subtopics[3]}</div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            ))}
+            ;{/* repeat this */}
             {/* repeat closes here */}
-
-            <div className="block">
-              <div className="topic">
-                <div className="circle"></div>
-                <div className="outer_border">
-                  <div className="title border">
-                    Math
-                    <img className="pencil" src={PencilImg} />
-                  </div>
-                  <div className="sub_topics border">
-                    <div className="sub_topic">math1</div>
-                    <div className="sub_topic">math2</div>
-                    <div className="sub_topic">math3</div>
-                    <div className="sub_topic">math4</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="block">
-              <div className="topic">
-                <div className="circle"></div>
-                <div className="outer_border">
-                  <div className="title border">
-                    Math
-                    <img className="pencil" src={PencilImg} />
-                  </div>
-                  <div className="sub_topics border">
-                    <div className="sub_topic">math1</div>
-                    <div className="sub_topic">math2</div>
-                    <div className="sub_topic">math3</div>
-                    <div className="sub_topic">math4</div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
         {/* {!!recommendedPosts.length && (
